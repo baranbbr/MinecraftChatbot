22a4afc8c52f0698a2fad1c048dde1286389b2e7
Jakub_Brodecki_Lenovo <jakub.brodecki@yahoo.com>
2019-10-17 13:40:15 +0100

    Connected to API with list of Minecrafts items and look for them in sentence

--- meaning.py
+++ meaning.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+import urllib.request, json
+
 class Meaning:
     """By passing refactored input this class calculate the meaning of sentence
        Simple algorithm will predict what user want to know
@@ -5,39 +9,42 @@ class Meaning:
     def __init__(self):
         self.classifier_words = {
             "Craft_question": ['how', 'do', 'does,', 'would', 'can', 'could',
-                               'I', 'is', 'be', 'was', 'were', 'to',
+                               'i', 'is', 'be', 'was', 'were', 'to',
                                'craft', 'crafted', 'make', 'made',
                                'solution', 'recipe',
                                'know', 'about'],
             "Item_question":  ['what', 'can', 'could',
-                               'I', 'is', 'be',
-                               'do', 'did', 'done',
+                               'i', 'is', 'be',
+                               'do', 'did', 'done', 'like',
                                'with'],
-            "Greeting":       ['Hi', 'Hello', 'Good', 'Morning', 'Afternoon', 'Yo',
-                               'Hey']
+            "Greeting":       ['hi', 'hello', 'good', 'morning', 'afternoon', 'yo',
+                               'hey']
         }
 
-    def predict(self, sentence_words, sentence_words_count):
+        self.items_names = list()
+        with urllib.request.urlopen("http://minecraft-ids.grahamedgecombe.com/items.json") as url:
+            data = json.loads(url.read().decode())
+            for item in data:
+                self.items_names.append(item['name'].lower())
+            print(self.items_names)
+
+    def predict(self, refactored_sentence, unrefactored_sentence):
         max_local = 0
         max_global = 0
         final_type = None
         confidence = 0
         for key in self.classifier_words:
-            iterator = 0
-            for word in sentence_words:
-                if word in self.classifier_words.get(key):
-                    max_local = max_local + 1 + sentence_words_count[iterator]
-                iterator = iterator + 1
+            for word in refactored_sentence[0]:
+                if word[0] in self.classifier_words.get(key):
+                    max_local = max_local + 1 + word[1]
             if max_local > max_global:
                 confidence = max_local - max_global
                 max_global = max_local
                 final_type = key
-        return final_type, confidence
+            max_local=0
 
-meaning = Meaning()
-print(
-meaning.predict(
-    ["How", "to", "create", "axe"],
-    [1,1,1,1]
-)
-)
+        item_name = None
+        for item in self.items_names:
+            if item in unrefactored_sentence and item_name is None:
+                item_name = item
+        return final_type, item_name, confidence
