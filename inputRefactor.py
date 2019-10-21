e5d31b72c691fec0442e807c3b35e855a4eb34e1
b-a-n-f <baranbaburisme@hotmail.com>
2019-10-21 15:42:27 +0100

    added wordtype ammended variable names

--- inputRefactor.py
+++ inputRefactor.py
@@ -1,15 +1,16 @@
 import numpy as np
+import nltk
 
+from nltk.tokenize import PunktSentenceTokenizer
 
-class InputRefactor:
+class inputRefactor:
 
     @classmethod
-    def nonLetterRemover(cls, sentence):
+    def nonLetterRemover(self, sentence):
         '''Removes Unecessary Commas and Determines if it is a question'''
-        exclusion_characters = [";", ":", "!", "!", "#", ",", ".", "@", "'", "£", "$", "%", "^", "&", "*", "(", ")",
-                                "\"", "{", "}", "]", "[", "~", "¬", "`"]
+        exclusion_characters = [";",":","!","!","#",",",".","@","'","£","$","%","^","&","*","(",")","\"","{","}","]","[","~","¬","`"]
         new_sentence_array = []
-        is_question = False  # initialising isQuestion as false
+        is_question = False  # initialising is_question as false
         sentence = sentence.lower()
         for word in sentence.split():  # getting each word individually
             new_word_array = []
@@ -25,26 +26,68 @@ class InputRefactor:
         return new_sentence_array, is_question
 
     @classmethod
-    def tokenise(cls, sentence):
+    def tokenise(self, sentence):
         '''Stores Data in 2D array to determine its ID and amount of times it occurs'''
         is_question = sentence[1]
-        sentence = sentence[0]  # splitting input
+        sentence = sentence[0] #splitting input
         word = []
         times = []
-        while len(sentence) > 0:
+        while len(sentence)>0:
             word_count = 0
             temp_word = sentence[0]
             sentence_constant = sentence
-            for j in sentence_constant:  # getting all unique words
+            for j in sentence_constant: #getting all unique words
                 if temp_word == j:
                     sentence.remove(j)
                     word_count += 1
             word.append(temp_word)
             times.append(word_count)
         data_store = np.zeros(len(word), dtype={'names': ('word', 'times'),
-                                                'formats': (
-                                                    'U46',
-                                                    'i4')})  # initialising data store with formats of data types.
+                                        'formats': ('U46', 'i4')})  # initialising data store with formats of data types.
         data_store['word'] = word
-        data_store['times'] = times  # adding all unique words and their occurrence to an associative array
+        data_store['times'] = times # adding all unique words and their occurrence to an associative array
         return data_store, is_question
+
+    @classmethod
+    def classifyWords(self, word_array):
+        '''Adds word-type (noun,verb) to existing 2D array format'''
+        is_question = word_array[1]
+        word_array = word_array[0]
+        word= []
+        times = []
+        data_store = np.zeros(len(word_array), dtype={'names': ('word', 'times','types'),
+                                               'formats': ('U46', 'i4', 'U46')})
+        count = 0
+        while len(word_array) > count:
+            word.append(word_array['word'][count])
+            times.append(word_array['times'][count])
+            count+= 1
+        types = inputRefactor.processWordType(word)
+        data_store['word'] = word
+        data_store['types'] = types
+        data_store['times'] = times #adding all unique words and their occurrence to an associative array
+        return data_store, is_question
+
+    @classmethod
+    def processWordType(self, sentence): #takes sentence inputs and finds word type
+        type_list= []
+        red_dict = {"CC":"NULL","CD":"NULL","DT":"NULL","EX":"NULL","FW":"NULL","IN":"NULL","JJ":"ADJECTIVE",
+                   "JJR":"ADJECTIVE","JJS":"ADJECTIVE","LS":"NULL","MD":"NULL","NN":"NOUN","NNS":"NOUN",
+                   "NNP":"NOUN","NNPS":"NOUN","PDT":"NULL","POS":"NULL","PRP":"PERSONAL","PRP$":"PERSONAL",
+                   "RB":"ADVERB","RBR":"ADVERB","RBS":"ADVERB","RP":"ADVERB","TO":"NULL","UH":"NULL",
+                   "VB":"VERB","VBD":"VERB","VBG":"VERB","VBN":"VERB","VBP":"VERB","VBZ":"VERB","WDT":
+                   "PRONOUN","WP":"PRONOUN","WP$":"PRONOUN","WRB":"PRONOUN"} #dictionary of reference for codes, useless ones set to NULL
+        for i in sentence:
+            words = nltk.word_tokenize(i) #setting each word to the NLTK ID
+            tagged = nltk.pos_tag(words) #getting the code associated with the ID
+            type_list.append(red_dict[tagged[0][1]]) #adding the referenced word in the dic above to an array
+        return(type_list)
+
+tempSentence = ("the cow watched the far land, for a small child. Cow the far land kept old men attacked? The cows were lost.")
+
+test = inputRefactor.nonLetterRemover(tempSentence)
+test1 = inputRefactor.tokenise(test)
+test2 = inputRefactor.classifyWords(test1)
+print(test2)
+
+
