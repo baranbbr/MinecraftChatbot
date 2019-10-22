348b3d9f8f9c91f00709d0970922f7634a91ba31
Jakub_Brodecki_Lenovo <jakub.brodecki@yahoo.com>
2019-10-22 12:06:22 +0100

    refactor spaghetti barans code

--- webscraping.py
+++ webscraping.py
@@ -2,39 +2,28 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.minecraftcraftingguide.net'
-response = requests.get(url)
-print(response)
 
-soup = BeautifulSoup(response.text, "html.parser")
 # found = soup.find_all("img")
 # print(found)
 
-item = "wood"
-item_regex = r"\b"+item+r"\b"
-# Find how to craft item
-for i in range(0, len(soup.find_all('img')) + 1):
-    try:
-        data = soup.find_all('img')[i]
-        # print(data)
-        img_title = data['alt']
-        print("img title is => " + str(img_title))
-        img_link = data['src']
-        contains_item = re.compile(item_regex, re.IGNORECASE)
-        contains_crafting = re.compile('crafting', re.IGNORECASE)
-        if contains_item.search(img_title) and contains_crafting.search(img_title):
-            print("je suis " + str(img_title))
-            print("This is how to craft a", item, "http://" + str(img_link)[2:])
-            break
-    except IndexError:
-        print(str(item) + " could not be found")
+class GetWebInfo:
+    def __init__(self):
+        url = 'https://www.minecraftcraftingguide.net'
+        response = requests.get(url)
+        self.soup = BeautifulSoup(response.text, "html.parser")
 
-
-# What can I do with item?
-# Need to look at ingredients then possibly span?
-# for i in range(0, len(soup.find_all(item))+1):
-#     try:
-#         data = soup.find_all(item)[i]
-#         print(data)
-#     except IndexError:
-#         print("error")
+    def get_craft_info(self, item):
+        item_regex = r"\b" + item + r"\b"
+        # Find how to craft item
+        for i in range(0, len(self.soup.find_all('img')) + 1):
+            try:
+                data = self.soup.find_all('img')[i]
+                # print(data)
+                img_title = data['alt']
+                img_link = data['src']
+                contains_item = re.compile(item_regex, re.IGNORECASE)
+                contains_crafting = re.compile('crafting', re.IGNORECASE)
+                if contains_item.search(img_title) and contains_crafting.search(img_title):
+                    return str(img_link)[2:]
+            except IndexError:
+                print(str(item) + " could not be found")
